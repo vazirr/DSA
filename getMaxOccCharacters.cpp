ed35b422790b6a0ff0e0d8f6538e919a8b1dd428
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-05-01 11:34:30 +0530

    Create getMaxOccCharacters.cpp

---
+++ getMaxOccCharacters.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+char getMaxOccCharacter(string s){
+    int arr[26] ={0};
+    //create an array of count of characters
+    for(int i = 0; i<s.length(); i++){
+        char ch = s[i];
+        int number = 0;
+        //lower case
+        if(ch >= 'a' && ch <='z'){
+            number = ch - 'a';
+        }
+        //upper case ko
+        else{
+            number = ch - 'A';
+            
+            
+        }
+        arr[number]++;
+    } 
+    // find maximum occuring element
+    int maxi = -1, ans = 0;
+    for(int i =0; i<26; i++){
+        if(maxi<arr[i]){
+            ans = i;
+            maxi = arr[i];
+        }
+       
+    }
+    char finalAns = 'a'+ans;
+    return finalAns;
+}
+
+int main()
+{
+    string s;
+   cin >> s;
+   cout << getMaxOccCharacter(s) << endl;
+ }
