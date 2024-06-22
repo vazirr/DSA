2c0bb2cf4b2531cf4e6137316fc5cc31fbcb5546
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-06-22 16:46:15 +0530

    Update cloneLLWithRandomPtr.cpp

--- cloneLLWithRandomPtr.cpp
+++ cloneLLWithRandomPtr.cpp
@@ -51,3 +51,94 @@ Solution
        
        
     }
+
+
+
+
+//approach 2: ----------------------------------------------------------------------------------------------------------------------------------------------------------
+private:
+    void insertAtTail(Node* &head, Node* &tail, int val){
+        
+        Node* newNode = new Node(val);
+        
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    public:
+    Node *copyList(Node *head)
+    {
+        //step 1 create a clone
+        
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        
+        Node* temp = head;
+        while(temp!= NULL){
+            insertAtTail(cloneHead,cloneTail,temp->data);
+            temp = temp->next;
+        }
+        
+             
+             
+        //step 2 : cloneNode add in between original Node
+        
+         Node* originalNode = head;
+         Node* cloneNode = cloneHead;
+         while(originalNode != NULL && cloneNode!=NULL){
+             Node* next = originalNode->next;
+             originalNode->next = cloneNode;
+             originalNode = next;
+             
+             next = cloneNode->next;
+             cloneNode->next = originalNode;
+             cloneNode = next;
+             
+         }
+        
+        //step 3 : copy the random ptr
+        temp = head;
+        while(temp != NULL){
+            if(temp->next != NULL){
+                if(temp->arb != NULL){
+                    temp->next->arb = temp->arb->next;
+                    
+                }else{
+                    temp->next = temp->arb;
+                }
+                
+                
+            }
+            
+            temp = temp->next->next;
+            
+            
+            
+            
+        }
+        //step 4 : revert the step 2
+        
+        originalNode = head;
+        cloneNode = cloneHead;
+        while(originalNode != NULL && cloneNode!=NULL){
+            originalNode -> next = cloneNode->next;
+            originalNode  = originalNode->next;
+            if(originalNode != NULL){
+                
+            cloneNode->next = originalNode ->next;
+                
+            }
+            
+            cloneNode = cloneNode->next;
+        }
+        
+        //step 5: return ans
+        return cloneHead;
+        
+    }
+
+};
