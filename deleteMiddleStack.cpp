aa35d9e7881f9f618ec28f7065a31bd29fe97bf7
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-06-25 17:04:04 +0530

    Create deleteMiddleStack.cpp

---
+++ deleteMiddleStack.cpp
@@ -0,0 +1,25 @@
+
+//page 12  --------------------------------------------------------------------------------------------------------------------------
+//newBook
+#include <bits/stdc++.h> 
+void solve(stack<int>& inputStack, int count, int size){
+   if(count == size/2){
+      inputStack.pop();
+      return;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   //recurrsive call
+   solve(inputStack, count+1, size);
+   inputStack.push(num);
+
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+  int count = 0;
+  solve(inputStack, count , N);
+   
+}
