e4bfa8d9a4b9decb90651bfe1108b5f82c456428
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-06-28 12:15:13 +0530

    Create minCostStringValid.cpp

---
+++ minCostStringValid.cpp
@@ -0,0 +1,39 @@
+//page 18 ------------------------------------------ newBook
+
+#include <bits/stdc++.h> 
+#include <stack>
+int findMinimumCost(string str) {
+  //odd condition
+  if(str.length()%2 != 0){
+    return -1;
+  }
+  stack<char> s;
+  for(int i = 0; i<str.length(); i++){
+    char ch = str[i];
+    if(ch == '{'){
+      s.push(ch);
+    }else{
+
+      //ch is close bracket
+       if(!s.empty() && s.top() == '{'){
+         s.pop();
+       }else{
+         s.push(ch);
+       }
+    }
+      
+  }
+  int a = 0;
+  int b = 0;
+
+      while(!s.empty()){
+        if(s.top() == '{'){
+          b++;
+        }else{
+          a++;
+        }
+        s.pop();
+      }
+      int ans = (a+1)/2 + (b+1)/2;
+  return ans;
+}
