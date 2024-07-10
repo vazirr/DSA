6dd097651b64ab8fdd7eff6523ecc6855f0ec377
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-07-10 17:35:39 +0530

    Create rotateArray.cpp

---
+++ rotateArray.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> ans(nums.size());
+
+        for(int i = 0; i< nums.size(); i++){
+            ans[(i+k)%nums.size()] = nums[i];
+        }
+
+        nums = ans;
+    }
+};
