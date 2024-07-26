666094885edc976c4d9dd4ec94cc63945c37135b
Shukla Vaibhav <91587491+vazirr@users.noreply.github.com>
2024-07-26 15:30:43 +0530

    Create LCAOfBST.cpp

---
+++ LCAOfBST.cpp
@@ -0,0 +1,36 @@
+//https://www.naukri.com/code360/problems/lca-in-a-bst_981280?leftPanelTab=0&leftPanelTabValue=SUBMISSION
+//page no 85
+/************************************************************
+    Following is the Binary Search Tree node structure
+
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+************************************************************/
+
+TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
+{
+    // Write your code here.
+    //base case
+    if(root == NULL){
+        return NULL;
+    }
+    if(root->data < P->data && root->data < Q->data){
+        return LCAinaBST(root->right, P, Q);
+    }
+     if(root->data > P->data && root->data > Q->data){
+        return LCAinaBST(root->left, P, Q);
+    }   
+    return root;
+    
+
+        
+
+}
